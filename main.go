fc8c859bf6d7248c005ab1158545855a55db88f6
Julien Etienne <j8@protonmail.ch>
2022-03-21 21:27:57 +0000

    Fixed array method Added slice method

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// Recursive method
+// Recursive method. (This can be very slow)
 func Recurse(n int) int {
 	if n <= 1 {
 		return n
@@ -12,6 +12,7 @@ func Recurse(n int) int {
 	return Recurse(n-1) + Recurse(n-2)
 }
 
+// Loop method.
 func Loop(n int) int {
 	if n <= 1 {
 		return n
@@ -27,16 +28,87 @@ func Loop(n int) int {
 	return c
 }
 
-func Prompt() {
-	fmt.Println("Provide the Fibonacci length")
+// Array Method.
+func Array(n int) int {
+	if n <= 1 {
+		return n
+	}
+	arr := make([]int, n)
+	arr[0], arr[1] = 1, 1
+
+	for i, _ := range arr {
+		if i < 2 {
+			continue
+		}
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+	return arr[len(arr)-1]
+}
+
+// Slice Method.
+func Slice(n int) int {
+	arr := make([]int, n+1, n+2)
+	if n < 2 {
+		arr = arr[0:2]
+	}
+
+	arr[0], arr[1] = 0, 1
+
+	for i := 2; i <= n; i++ {
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+	return arr[n]
+}
+
+func Prompt(method string) {
 	var fibonacciLength int
+	var result int
+	var methodName string
+
+	switch method {
+	case "r":
+		methodName = "Recursive"
+	case "a":
+		methodName = "Array"
+	case "l":
+		methodName = "Loop"
+	case "s":
+		methodName = "Slice"
+	}
+	fmt.Printf(`Provide the %v Fibonacci length: `, methodName)
 	fmt.Scanln(&fibonacciLength)
-	// fmt.Println(Recurse(fibonacciLength))
-	fmt.Println(Loop(fibonacciLength))
-	Prompt()
+
+	switch method {
+	case "r":
+		result = Recurse(fibonacciLength)
+	case "a":
+		result = Array(fibonacciLength)
+	case "l":
+		result = Loop(fibonacciLength)
+	case "s":
+		result = Slice(fibonacciLength)
+	}
+
+	fmt.Println(result)
+	Prompt(method)
 }
 
 func main() {
+	var fibonacciMethod string
+	fmt.Printf(`
 
-	Prompt()
+╔═╗┬┌┐ ┌─┐┌┐┌┌─┐┌─┐┌─┐┬
+╠╣ │├┴┐│ ││││├─┤│  │  │
+╚  ┴└─┘└─┘┘└┘┴ ┴└─┘└─┘┴
+	
+Choose a Fibonacci method
+
+- Recursive 			(r)
+- Loop				(l)
+- Array				(a)
+- Better Array			(s)
+(Type r/l/a/s): `)
+
+	fmt.Scanln(&fibonacciMethod)
+	Prompt(fibonacciMethod)
 }
