6a01464337b46f8c241c35dfa7a1b3d98aba1f22
Julien Etienne <j8@protonmail.ch>
2022-03-21 18:09:24 +0000

    Added fibonacci function using a for loop

--- main.go
+++ main.go
@@ -12,11 +12,27 @@ func Recurse(n int) int {
 	return Recurse(n-1) + Recurse(n-2)
 }
 
+func Loop(n int) int {
+	if n <= 1 {
+		return n
+	}
+	a, b := 0, 1
+	var c int
+
+	for i := 2; i <= n; i++ {
+		c = a + b
+		a = b
+		b = c
+	}
+	return c
+}
+
 func Prompt() {
 	fmt.Println("Provide the Fibonacci length")
 	var fibonacciLength int
 	fmt.Scanln(&fibonacciLength)
-	fmt.Println(Recurse(fibonacciLength))
+	// fmt.Println(Recurse(fibonacciLength))
+	fmt.Println(Loop(fibonacciLength))
 	Prompt()
 }
 
